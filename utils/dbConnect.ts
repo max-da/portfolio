5d68deee54ce50fe2b712c62b340fb4d34946950
max-da <max.dahlbom@outlook.com>
2021-12-21 16:31:38 +0100

    image uploading now working

--- utils/dbConnect.ts
+++ utils/dbConnect.ts
@@ -1,19 +1,32 @@
-import mongoose, {Model} from "mongoose";
+import mongoose, { Model, mongo } from "mongoose";
 
-const {DATABASE_URL} = process.env;
+const { DATABASE_URL } = process.env;
 
-export const connect = async ()=> {
-    const connection = await mongoose.connect(DATABASE_URL as string)
-    .catch((err)=> {console.log(err)})
-    console.log("Connected")
+export const connect = async () => {
+  const connection = await mongoose
+    .connect(DATABASE_URL as string)
+    .catch((err) => {
+      console.log(err);
+    });
+  console.log("Connected");
 
-    const projectSchema = new mongoose.Schema({
-        name: String,
-        
-    })
-    
-    const Project = mongoose.model("Project", projectSchema)
-    return {connection, Project}
+  const projectSchema = new mongoose.Schema({
+    name: {type:String, required:true},
+    techStack: {type:String, required:true},
+    description: {type:String, required:true},
+    createdDate: Date,
+    gitLink: String || null,
+    liveLink: String,
+    images: [
+      {
+        image: {
+          path: { type: String, required: true },
+        },
+      },
+    ],
+  });
 
-
-}
+  const Project =
+    mongoose.models.Project || mongoose.model("Project", projectSchema);
+  return { connection, Project };
+};
