5ed9ae082f1415258acd424af86b0f336383c166
max-da <max.dahlbom@outlook.com>
2022-01-21 14:23:45 +0100

    fix

--- utils/session.ts
+++ utils/session.ts
@@ -1,18 +1,16 @@
 
-// this file is a wrapper with defaults to be used in both API routes and `getServerSideProps` functions
 import type { IronSessionOptions } from 'iron-session'
 
 
 export const sessionOptions: IronSessionOptions = {
   password: process.env.IRON_SESSION as string,
   cookieName: "acack",
-  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
   },
 }
 
-// This is where we specify the typings of req.session.*
+
 declare module 'iron-session' {
   interface IronSessionData {
     user?: boolean
